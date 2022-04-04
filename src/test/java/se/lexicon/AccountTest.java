61039306151f4e5d760cefdb79aadc30c89fe0b8
Mehrdad Javan <mehrdad.javan@adapta.se>
2022-04-04 11:30:54 +0200

    use @Before annotation to improve the cods

--- src/test/java/se/lexicon/AccountTest.java
+++ src/test/java/se/lexicon/AccountTest.java
@@ -1,30 +1,31 @@
 package se.lexicon;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
 
+  Account account;
+
+  @Before
+  public void setUp(){
+     account = new Account();
+  }
+
   @Test
   public void test_deposit() {
-    Account account = new Account();
-
     double depositAmount = 200;
     double expectedBalance = 200;
-
     account.deposit(depositAmount);
     Assert.assertEquals(expectedBalance, account.getBalance(), 0);
   }
 
   @Test
   public void test_withdraw() {
-    Account account = new Account();
     account.deposit(200);
-
     double expectedBalance = 150;
-
     double actualBalance = account.withdraw(50);
-
     Assert.assertEquals(expectedBalance, actualBalance, 0);
   }
 }
